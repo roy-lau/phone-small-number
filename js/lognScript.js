b3c8e2c22598c17e355cf84bfdd230e118660508
a-qiang <13526636962@163.com>
2016-12-21 12:08:27 +0800

    clear focus blur

--- js/lognScript.js
+++ js/lognScript.js
@@ -1,9 +1,3 @@
 $(document).on("pagecreate", "#Pagelogn", function() {
-	// $('input').on('focus',function(){
-	//     $('input').css({'position':'static'})
 
-	// });
-	// $('input').on('blur',function(){
-	//     $('input').css({'position':'fixed'})
-	// });
 });
