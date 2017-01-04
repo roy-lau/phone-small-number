af7c0a4b0abbe962368a43596df79f3d299763f8
a-qiang <13526636962@163.com>
2017-01-04 15:21:00 +0800

    On branch gh-pages   Your branch is up-to-date with 'origin/gh-pages'.  Changes to be committed: 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).on("pageinit", "#Pagelogn", function() {
         if (blm == false) {
             $("#logn").attr("href", "#bindingPage")
             $("#logn").next().html("")
-        } else if(name) {
+        } else if (name) {
             $("#logn").attr("href", "#page1")
             $("#logn").next().html("")
         } else {
@@ -15,23 +15,23 @@ $(document).on("pageinit", "#Pagelogn", function() {
     });
 });
 $(function() {
-      var number = [
-        {"tel": 13598412712},
-        {"tel": 13522222211},
-        {"tel": 13533333333},
-        {"tel": 17055555557},
-        {"tel": 13598412712},
-        {"tel": 17066666666},
-        {"tel": 17177777777},
-        {"tel": 17188888888},
-        {"tel": 17199999999}
-      ]
-    
+    var number = [
+        { "tel": 18001351358 },
+        { "tel": 13371698888 },
+        { "tel": 18911919999 },
+        { "tel": 13301111109 },
+        { "tel": 18010125555 },
+        { "tel": 18911731688 },
+        { "tel": 13311119703 },
+        { "tel": 18911970588 },
+        { "tel": 18911731588 }
+    ]
+
     for (var i = 0; i < number.length; i++) {
         $("select[name='phone']").append("<option value=" + number[i].tel + " >" + number[i].tel + "</option>")
         $("select[name='sms']").append("<option value=" + number[i].tel + " >" + number[i].tel + "</option>")
     }
-    
+
     $(document).on("pageinit", "#page1", function() {
         $("#TelBtn").on("click", function() {
             $("#TelBtn").attr("href", "tel:" + $("select[name='phone']").find("option:selected").val());
@@ -43,19 +43,18 @@ $(function() {
             $("#SmsBtn").attr("href", "sms:" + $("select[name='sms']").find("option:selected").val() + "?body=" + TextArea);
         });
     });
+
 });
 
 $(document).on("pageinit", "#page3", function() {
     var obj = [
-        { "tel": 135, "date": "2016-12-1" }, { "tel": 136, "date": 2016 - 12 - 2 }, { "tel": 138, "date": 2016 - 12 - 3 },
-        { "tel": 170, "date": "2016-12-4" }, { "tel": 188, "date": 2016 - 12 - 5 }, { "tel": 166, "date": 2016 - 12 - 6 },
-        { "tel": 171, "date": "2016-12-7" }, { "tel": 155, "date": 2016 - 12 - 8 }, { "tel": 169, "date": 2016 - 12 - 9 }
+        { "tel": 135, "date": "2016-12-1" }, { "tel": 136, "date": "2016-12-2" }, { "tel": 138, "date": "2016 - 12 - 3" },
+        { "tel": 170, "date": "2016-12-4" }, { "tel": 188, "date": "2016-12-5" }, { "tel": 166, "date": "2016 - 12 - 6" },
+        { "tel": 171, "date": "2016-12-7" }, { "tel": 155, "date": "2016-12-8" }, { "tel": 169, "date": "2016 - 12 - 9" }
     ]
     for (var i = 0; i < obj.length; i++) {
         $('#UlTel').append("<li data-icon='info' class='ui-last-child'>" + "<a href='tel:13526636962' class='ui-btn ui-btn-icon-right ui-icon-info'>" + "<h4>" + obj[i].tel + "</h4>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" + "</a>" + "</li>")
-
     }
-
 });
 $(document).on("pageinit", "#page4", function() {
 
