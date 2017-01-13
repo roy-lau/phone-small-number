0de10c8a5dd67035fe8068ac9c7e6d35ab55ad95
a-qiang <13526636962@163.com>
2017-01-13 17:24:21 +0800

    hui

--- js/script.js
+++ js/script.js
@@ -1,20 +1,97 @@
-fixScale = function(doc) {
+$(document).on("pageinit", "#Pagelogn", function() {
+    $("#logn").on("click", function() {
+        var name = $('#userName').val();
+        if (name) {
+            $("#logn").attr("href", "#page1")
+            $("#logn").next().html("")
+        } else {
+            $("#logn").attr("href", "")
+            $("#logn").next().html("用户名密码不匹配！")
+        }
+    });
+});
 
-	var addEvent = 'addEventListener',
-	    type = 'gesturestart',
-	    qsa = 'querySelectorAll',
-	    scales = [1, 1],
-	    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+$(document).on("pageinit", "#page1", function() {
 
-	function fix() {
-		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-		doc.removeEventListener(type, fix, true);
-	}
+});
+$(document).on("pageinit", "#page2", function() {
+$('#link').on('click', function() {
+            $.mobile.changePage("#Addlinkman",{transition:"pop"});//$('#Addlinkman').popup('open');
+        });
 
-	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-		fix();
-		scales = [.25, 1.6];
-		doc[addEvent](type, fix, true);
-	}
+    var obj = [
+        { "name": "user1", "tel": 135, "date": "2016-12-1" }, { "name": "user2", "tel": 136, "date": "2016-12-2" }, { "name": "user3", "tel": 138, "date": "2016-12-3" },
+        { "name": "user1", "tel": 170, "date": "2016-12-4" }, { "name": "user2", "tel": 188, "date": "2016-12-5" }, { "name": "user3", "tel": 166, "date": "2016-12-6" },
+        { "name": "user1", "tel": 171, "date": "2016-12-7" }, { "name": "user2", "tel": 155, "date": "2016-12-8" }, { "name": "user3", "tel": 169, "date": "2016-12-9" }
+    ]
 
-};
+    for (var i = 0; i < obj.length; i++) {
+        $('#UlTel').append("<li data-icon='info' class='ui-last-child'>" 
+            + "<a class='ui-btn ui-btn-icon-right ui-icon-info'   href='wtai://wp/mc;" + obj[i].tel + "'>" 
+            + "<h4>" + obj[i].name + "</h4>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" + "</a>" + "</li>")
+    }
+
+});
+
+$(document).on("pageinit", "#page3", function() {
+    var number = [{ "tel": 13100501490 }, { "tel": 13371698888 }, { "tel": 18911919999 }, { "tel": 13301111109 }, { "tel": 18010125555 }, { "tel": 18911731688 }, { "tel": 13311119703 }, { "tel": 18911970588 }, { "tel": 18911731588 }]
+    for (var i = 0; i < number.length; i++) {
+        $("select[name='phone']").append("<option value=" + number[i].tel + " >" 
+            + number[i].tel + "</option>")
+    }
+    $("#TelBtn").on("click", function() {
+        $("#TelBtn").attr("href", "tel:" + $("select[name='phone']").find("option:selected").val());
+
+        switch (model) {
+            case 1:
+                $("select[name='phone']").find("option:selected").val()
+                break;
+            case 2:
+                $("select[name='phone']").find("option:selected").val()
+                break;
+            case 3:
+                $("select[name='phone']").find("option:selected").val()
+                break;
+            default:
+        }
+
+    });
+
+});
+$(document).on("pageinit", "#page4", function() {
+    var name = $('#userName').val();
+    $("#machine").html(name)
+});
+
+$(document).on("pageinit", "#linkmanList", function() {
+
+    var obj = [
+        { "name": "user1", "tel": 135, "date": "2016-12-1" }, { "name": "user2", "tel": 136, "date": "2016-12-2" }, { "name": "user3", "tel": 138, "date": "2016-12-3" },
+        { "name": "user1", "tel": 170, "date": "2016-12-4" }, { "name": "user2", "tel": 188, "date": "2016-12-5" }, { "name": "user3", "tel": 166, "date": "2016-12-6" },
+        { "name": "user1", "tel": 171, "date": "2016-12-7" }, { "name": "user2", "tel": 155, "date": "2016-12-8" }, { "name": "user3", "tel": 169, "date": "2016-12-9" }
+    ]
+
+    for (var i = 0; i < obj.length; i++) {
+        $('#UlTel').append("<li data-icon='info' class='ui-last-child'>" + "<a class='ui-btn ui-btn-icon-right ui-icon-info'   href='tel:" + obj[i].tel + "'>" + "<h4>" + obj[i].name + "</h4>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" + "</a>" + "</li>")
+    }
+
+});
+
+$(document).on("pageinit", "#reset", function() {
+    $("#submit").on("click", function() {
+        var newPwd = $('#newPwd').val();
+        var oldPwd = $('#oldPwd').val();
+        if (newPwd == oldPwd) {
+            //在这里写发送的方法！
+            $('#submit').removeAttr('disabled');
+            $("#submit").attr("href", "#Pagelogn")
+            alert("密码修改成功！")
+        } else {
+            $('#submit').attr('disabled', 'disabled');
+            $("#submit").attr("href", "#")
+            alert("两次密码输入不一致！")
+        }
+    })
+});
+
+
