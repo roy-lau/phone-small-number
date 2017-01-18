768ef2aec70f63be99061f52731b1841886394e3
a-qiang <13526636962@163.com>
2017-01-18 11:52:57 +0800

    color

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var path;
 $(document).on("pageinit", "#Pagelogn", function() {
     $("#logn").on("click", function() {
         var name = $('#userName').val();
@@ -8,15 +9,15 @@ $(document).on("pageinit", "#Pagelogn", function() {
             $("#logn").prop("href", "")
             $("#logn").next().html("用户名密码不匹配！")
         }
-        switch (model) {
+        switch (2) {
             case 1:
-
+                path = "url1"
                 break;
             case 2:
-
+                path = "url2"
                 break;
             case 3:
-
+                path = "url3"
                 break;
             default:
         }
@@ -25,7 +26,7 @@ $(document).on("pageinit", "#Pagelogn", function() {
 
 $(document).on("pageinit", "#page1", function() {
     /******当有通话记录的时候显示数据，没有通话记录的时候显示“亲，您还没有通话记录哟！”*******/
-    if (1==1) { 
+    if (1 == 1) {
         $("#LatelyList").append("<p style='text-align: center;line-height: 10em'>亲，您还没有通话记录哟！</p>")
     } else {
         var obj = [
@@ -37,8 +38,8 @@ $(document).on("pageinit", "#page1", function() {
             $('#LatelyList').append("<li data-icon='info' class='ui-last-child'>" + "<a class='ui-btn ui-btn-icon-right ui-icon-info'  href='#details'>" + "<h4 value=" + obj[i].tel + " >" + obj[i].name + "</h4>" + "<p>" + obj[i].tel + "</p>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" + "</a>" + "</li>")
         }
     }
-
 });
+
 $(document).on("pageinit", "#page2", function() {
     $('#link').on('click', function() {
         $.mobile.changePage("#Addlinkman", { transition: "pop" }); //$('#Addlinkman').popup('open');
@@ -62,21 +63,22 @@ $(document).on("pageinit", "#page2", function() {
         $('#linkmanTotal').append("<li data-icon='info' class='ui-last-child'>" + "<a class='ui-btn ui-btn-icon-right ui-icon-info'  href='#details'>" + "<h4 value=" + obj[i].tel + " >" + obj[i].name + "</h4>" + "<p>" + obj[i].tel + "</p>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" + "</a>" + "</li>")
     }
     $("[href=#details]").on('click', function() {
-        alert($(this).find("p").html())             //点击某个联系人把号码传个后台
+        alert($(this).find("p").html())
     })
 });
 
 $(document).on("pageinit", "#page3", function() {
     $("a[href=#Addlinkman]").on("click", function() {
-        $("#Number1").val(EditPhone.innerHTML)      //点击添加联系人，把号码放到添加联系人的页面 Add
-    });     
+        $("#Number1").val(EditPhone.innerHTML) //点击添加联系人，把号码放到添加联系人的页面 Add
+    });
 
     $("#sms").on("click", function() {
         $(this).prop("href", "sms:" + EditPhone.innerHTML + "?body=");
     });
     $("#TelBtn").on("click", function() {
-        $(this).prop("href", "wtai://wp/mc;" + EditPhone.innerHTML);
+        $(this).prop("href", "wtai://wp/mc;" + EditPhone.innerHTML) || $(this).prop("href", "tel:" + EditPhone.innerHTML);
         EditPhone.innerHTML = ""
+        alert(path)
     });
 
     $("#Toggle").on("click", function() {
@@ -87,7 +89,6 @@ $(document).on("pageinit", "#page3", function() {
     $(".del").on("click", function() {
         EditPhone.innerHTML = EditPhone.innerHTML.substring(0, EditPhone.innerHTML.length - 1);
     });
-
 });
 
 $(document).on("pageinit", "#page4", function() {
@@ -98,21 +99,24 @@ $(document).on("pageinit", "#page4", function() {
 $(document).on("pageinit", "#Addlinkman", function() {
     $("#tolinkman").on("click", function() {
         var linkmanArr = [$("#UserName").val(), $("#Number1").val(), $("#Number2").val(), $("#company").val(), $("#Remarks").val()]
-        console.log(linkmanArr)     //添加联系人
+        console.log(linkmanArr) //添加联系人
     });
 });
 
 $(document).on("pageinit", "#linkmanList", function() {
     var obj = [
-        { "name": "user1", "tel": 135, "date": "2016-12-1" }, { "name": "user2", "tel": 136, "date": "2016-12-2" }, { "name": "user3", "tel": 138, "date": "2016-12-3" },
-        { "name": "user1", "tel": 170, "date": "2016-12-4" }, { "name": "user2", "tel": 188, "date": "2016-12-5" }, { "name": "user3", "tel": 166, "date": "2016-12-6" },
-        { "name": "user1", "tel": 171, "date": "2016-12-7" }, { "name": "user2", "tel": 155, "date": "2016-12-8" }, { "name": "user3", "tel": 169, "date": "2016-12-9" }
-    ]
-    //---------------某个月的联系记录
+            { "name": "user1", "tel": 135, "date": "2016-12-1" }, { "name": "user2", "tel": 136, "date": "2016-12-2" }, { "name": "user3", "tel": 138, "date": "2016-12-3" },
+            { "name": "user1", "tel": 170, "date": "2016-12-4" }, { "name": "user2", "tel": 188, "date": "2016-12-5" }, { "name": "user3", "tel": 166, "date": "2016-12-6" },
+            { "name": "user1", "tel": 171, "date": "2016-12-7" }, { "name": "user2", "tel": 155, "date": "2016-12-8" }, { "name": "user3", "tel": 169, "date": "2016-12-9" }
+        ]
+        //---------------某个月的联系记录
     for (var i = 0; i < obj.length; i++) {
-        $('#linkmanL').append("<li data-icon='info' class='ui-last-child'>" + "<a class='ui-btn ui-btn-icon-right ui-icon-info' href='tel:" + obj[i].tel + "'>" + "<h4>" + obj[i].name + "</h4>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" + "</a>" + "</li>")
+        $('#linkmanL').append("<li data-icon='info' class='ui-last-child'>" 
+            + "<a class='ui-btn ui-btn-icon-right ui-icon-info' href='tel:" + obj[i].tel + "'>" 
+            + "<h4>" + obj[i].name + "</h4>" 
+            + "<span class='ui-li-count'>" + obj[i].date + "</span>" 
+            + "</a>" + "</li>")
     }
-
 });
 
 $(document).on("pageinit", "#details", function() {
@@ -133,8 +137,8 @@ $(document).on("pageinit", "#details", function() {
         $("#RecordList").hide()
         alert("向右滑动!");
         // for (var i = 0; i < data.length; i++) {
-        //     $('#linkmanL').append("<li>" 
-        //         + "<a href=''>" +  + "</a>" + "</li>")
+        //     $('#RecordList').append("<li>" 
+        //         + "<a href=''>" +  + "</a>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" +  "</li>")
         // }
     });
     $("#Dl").on("click", function() {
@@ -152,27 +156,29 @@ $(document).on("pageinit", "#details", function() {
         $("#RecordList").show()
 
         // for (var i = 0; i < data.length; i++) {
-        //     $('#linkmanL').append("<li>" 
-        //         + "<a href=''>" +  + "</a>" + "</li>")
+        //     $('#RecordList').append("<li>" 
+        //         + "<a href=''>" +  + "</a>" + "<span class='ui-li-count'>" + obj[i].date + "</span>" +  "</li>")
         // }
     });
 
-    $("#Dtel").on("click", function(){
-       $(this).prop("href", "sms:" + $("#DNumber1").html() + "?body=");
+    $("#Dtel").on("click", function() {
+        $(this).prop("href", "sms:" + $("#DNumber1").html() + "?body=");
     })
-    $("#Dtel").on("click", function(){
-       $(this).prop("href", "wtai://wp/mc;" + $("#DNumber1").html()); 
+    $("#Dtel").on("click", function() {
+        $(this).prop("href", "wtai://wp/mc;" + $("#DNumber1").html());
     })
-    $("[href=#modify]").on("click", function(){
-        $("#DNumber1").html()   //点击编辑 把号码传给后台
+    $("[href=#modify]").on("click", function() {
+        $("#DNumber1").html() //点击编辑 把号码传给后台
     })
 });
+
 $(document).on("pageinit", "#modify", function() {
     $("#modifyLinkman").on("click", function() {
         var linkmanArr = [$("#UserName").val(), $("#Number1").val(), $("#Number2").val(), $("#company").val(), $("#Remarks").val()]
-        console.log(linkmanArr)    //修改某个联系人信息
+        console.log(linkmanArr) //修改某个联系人信息
     });
 });
+
 $(document).on("pageinit", "#reset", function() {
     $("#submit").on("click", function() {
         var newPwd = $('#newPwd').val();
