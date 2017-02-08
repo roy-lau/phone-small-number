73e9111d8bcefaefadd08cba243792061f507d55
Roy-Lau <13526636962@163.com>
2017-02-08 16:03:57 +0800

    test iphone sms

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ $(document).on("pageinit", "#page3", function() {
     });
 
     $("#sms").on("click", function() {
-        $(this).attr("href", "sms:" + EditPhone.innerHTML + "?body=" + EditPhone.innerHTML);
+        $(this).attr("href", "sms:" + EditPhone.innerHTML + ";body=" + EditPhone.innerHTML) || $(this).attr("href", "sms:" + EditPhone.innerHTML + "?body=" + EditPhone.innerHTML);
     });
     $("#TelBtn").on("click", function() {
         $(this).prop("href", "tel:" + EditPhone.innerHTML) || $(this).prop("href", "wtai://wp/mc;" + EditPhone.innerHTML)
